885d13f9c5f2059f0bdc7224c58df45509210df0
alamin247khan <167293333+alamin247khan@users.noreply.github.com>
2024-05-05 00:03:36 +0600

    fbgbvg

---
+++ 3. Java Script/Hello.js
@@ -0,0 +1,12 @@
+console.log(21);
+console.log('good morning')
+var bananaPrice = 50;
+console.log(bananaPrice);
+
+var name = "alamin khan"
+console.log(typeof name)
+
+var isHot = true;
+var isRice = false;
+console.log(typeof isHot)
+
