392ce80390c1f46c25dc947a633ce1bfc32c695d
Al Amin Khan <alamin24.7khan@outlook.com>
2024-05-14 17:08:05 +0600

    Update main.py

--- 4. Python/main.py
+++ 4. Python/main.py
@@ -1,31 +1,33 @@
 #String Methods
-#name = "Bro Code"
-#print(len(name))
-#print(name.find("r"))
-#print(name.capitalize())
-#print(name.upper())
-#print(name.lower())
-#print(name.isdigit())
-#print(name.isalpha())
-#print(name.count("o"))
-#print(name.replace("o","a"))
-#print(name*4)
+name = "i love my country"
+print(len(name))
+print(name.find("r"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("o"))
+print(name.replace("o","a"))
+print(name*4)
 
 #Type Casting = Convert the data type of a value to onather data type.
-#x = 1 #int
-#y = 2.0 #float
-#z = "3" #str
-#x = float(x)
-#y = int(y)
-#z = int(z)
-#z = str(z)
+x = 1 #int
+y = 2.0 #float
+z = "3" #str
+x = float(x)
+y = int(y)
+z = int(z)
+z = str(z)
 #print(x)
-#print("y is " + str(y))
-#print(z*3)
+print("y is " + str(y))
+print(z*3)
 
 
 name = input("What is your name?: ")
 age = int(input("who old are you?: "))
 age = age + 1
-print("Hello "+ name)
-print("You are " + str(age) + " years old")
+height = input("what is your hight?: ")
+print("My name is "+ name)
+print("I am " + str(age) + " years old")
+print("my height is "+ height)
